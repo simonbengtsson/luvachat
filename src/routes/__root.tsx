dbcf81208577dd62a3b5a2331309499f6296d903
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2026-03-14 16:28:59 +0100

    Update links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,6 +22,10 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
+        name: "apple-mobile-web-app-title",
+        content: "Luvachat",
+      },
+      {
         title: "Luvachat",
       },
     ],
@@ -29,6 +33,26 @@ export const Route = createRootRoute({
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
       },
     ],
   }),
