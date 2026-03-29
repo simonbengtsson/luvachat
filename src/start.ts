7779567e0b5576c06ba5134392c75027d4f4b3b5
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2026-03-29 21:20:55 +0200

    Add error logging

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,18 @@
-import { createStart } from "@tanstack/react-start"
+import { logRequestError } from "@/core/error-reporting"
+import { createMiddleware, createStart } from "@tanstack/react-start"
+
+const requestErrorLogger = createMiddleware().server(
+  async ({ next, request }) => {
+    try {
+      return await next()
+    } catch (error) {
+      logRequestError("tanstack.request", request, error)
+      throw error
+    }
+  },
+)
 
 export const startInstance = createStart(() => ({
   defaultSsr: false,
+  requestMiddleware: [requestErrorLogger],
 }))
