6d11f15700872b04fa7f68c5910bccade1980747
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2026-04-08 09:57:04 +0200

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
-import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -13,7 +12,7 @@ const config = defineConfig({
     ),
   },
   plugins: [
-    devtools(),
+    //devtools(),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
